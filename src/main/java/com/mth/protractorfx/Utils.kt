949d011eaed6ec3fd1a5e4cd5ed285e010c0707c
matti <sesquialtera87@gmail.com>
2022-07-22 12:08:53 +0200

    Shortcuts and cursors

--- src/main/java/com/mth/protractorfx/Utils.kt
+++ src/main/java/com/mth/protractorfx/Utils.kt
@@ -1,16 +1,44 @@
 package com.mth.protractorfx
 
 import javafx.geometry.Point2D
+import javafx.scene.Cursor
 import javafx.scene.ImageCursor
 import javafx.scene.image.Image
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyCodeCombination
 import java.io.File
 import kotlin.math.atan2
+
+
+@JvmField
+val SHORTCUT_DELETE = KeyCodeCombination(KeyCode.DELETE)
+
+@JvmField
+val SHORTCUT_DELETE_SWITCH = KeyCodeCombination(KeyCode.X)
+
 
 @JvmField
 val CURSOR_ANGLE = ImageCursor(Image(ImageProtractor::class.java.getResourceAsStream("angle_x32.png")))
 
 @JvmField
+val CURSOR_INSERT_DOT = ImageCursor(Image(ImageProtractor::class.java.getResourceAsStream("insert_cursor_x32.png")))
+
+@JvmField
+val CURSOR_REMOVE_DOT = ImageCursor(Image(ImageProtractor::class.java.getResourceAsStream("delete_cursor_x32.png")))
+
+
+@JvmField
 val SNAPSHOT_DIR = File("C:\\Users\\matti\\OneDrive\\Documenti\\Java\\ProtractorFX\\src\\main\\resources\\snapshot")
+
+@JvmField
+var dotInsertionEnabled = false
+
+@JvmField
+var dotDeletionEnabled = false
+
+@JvmField
+var angleMeasureEnabled = false
+
 
 fun angleBetween(p1: Point2D, p2: Point2D, degree: Boolean = false): Double {
     val dot = p1.x * p2.x + p1.y * p2.y
@@ -18,7 +46,7 @@ fun angleBetween(p1: Point2D, p2: Point2D, degree: Boolean = false): Double {
     var angle = atan2(det, dot)
 
     if (degree) {
-        // converto to degree
+        // convert to degree
         angle = Math.toDegrees(angle)
 
         // put angle measure in range [0,360]
