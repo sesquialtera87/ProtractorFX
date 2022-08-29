f6f11e2769e391d789fb8067d253cd1979398e4a
mattia <sesquialtera87@gmail.com>
2022-08-29 16:23:04 +0200

    Comments improved

--- src/main/java/org/mth/protractorfx/Selection.kt
+++ src/main/java/org/mth/protractorfx/Selection.kt
@@ -4,25 +4,43 @@ import java.util.*
 
 object Selection {
 
+    /**
+     * The number of dots actually selected
+     */
     val size: Int get() = selectedDots().size
 
+    /**
+     * Select the [dot], clearing all other previous selected nodes
+     */
     fun select(dot: Dot) {
         clear()
         addToSelection(dot)
     }
 
+    /**
+     * Remove the [dot] from the selection
+     */
     fun unselect(dot: Dot) {
         dot.selected = false
     }
 
+    /**
+     * Add the [dot] to the selection, maintaining the previously selected dots.
+     */
     fun addToSelection(dot: Dot) {
         dot.selected = true
     }
 
+    /**
+     * Add to the selection all the dots belonging to the [dotChain], preserving the previously selected dots.
+     */
     fun addToSelection(dotChain: DotChain) {
         dotChain.forEach { it.selected = true }
     }
 
+    /**
+     * Remove all dots in the selection
+     */
     fun clear() {
         chains.flatten().forEach {
             it.selected = false
@@ -33,5 +51,8 @@ object Selection {
         Optional.of(selectedDots().first())
     else Optional.empty()
 
+    /**
+     * Return a collection of the current selected dots
+     */
     fun selectedDots() = chains.flatten().filter { it.selected }
 }
